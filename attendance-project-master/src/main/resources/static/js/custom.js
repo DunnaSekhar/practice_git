f08f0d8792d4dfc447768a82455adcf4d6c322b1
GCS\sdunna <Dunna.Sekhar@amwater.com>
2023-02-14 15:05:23 +0530

    push

---
+++ attendance-project-master/src/main/resources/static/js/custom.js
@@ -0,0 +1,16 @@
+/**
+ * program name : commonJsFunction.js
+ * Author       : R.Emmanuel Raj
+*/
+
+
+/**
+ * while blur of confirmPassword the  
+ */
+
+console.log(window);
+
+function hi()
+{
+  alert('hi');	
+}
